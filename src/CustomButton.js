a78c8524ae80ff8cbaa3a25d5f92b3fff47c7b1a
AyseKalkan <aysekalkan.tr@gmail.com>
2023-08-18 12:41:44 +0300

    Example

---
+++ src/CustomButton.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { Pressable, StyleSheet, Text, TouchableOpacity } from "react-native";
+
+const CustomisedButton =(props) => {
+    
+  return (
+    <Pressable
+      style={({ pressed }) => [
+        { backgroundColor: pressed ? '#dddddd' : props.color },
+        styles.button,
+        {...props.style}
+      ]}
+      onPress={props.onPressFunction}>
+      <Text style={styles.text}>{props.title}</Text>
+    </Pressable>
+  );
+};
+
+const styles = StyleSheet.create({
+
+    text: {
+      color: '#000000',
+      fontSize: 20,
+      margin : 10,
+      textAlign: 'center',
+    },
+   
+    button: {
+      borderRadius:5,
+      alignItems : 'center',
+    },
+    
+    
+  });
+export default CustomisedButton
