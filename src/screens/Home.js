7eaeed472b5de6f87f3f424329fb7f7d94692837
AyseKalkan <aysekalkan.tr@gmail.com>
2023-08-21 21:43:55 +0300

    Example

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,142 +1,138 @@
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import {
-    StyleSheet,
-    View,
-    Text,
-    Alert,
-    TextInput,
+  StyleSheet,
+  View,
+  Text,
+  Pressable,
+  Alert,
 } from 'react-native';
-import CustomButton from '../utils/CustomButton';
 import GlobalStyle from '../utils/GlobalStyle';
-import SQLite from 'react-native-sqlite-storage';
-
-const db = SQLite.openDatabase(
-    {
-        name: 'MainDB',
-        location: 'default',
-    },
-    () => { },
-    error => { console.log(error) }
-);
+import { TextInput } from 'react-native-gesture-handler';
+import CustomisedButton from '../utils/CustomButton';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function Home({ navigation, route }) {
 
-    const [name, setName] = useState('');
-    const [age, setAge] = useState('');
+    
+  const [name, setName] = useState('');
+  const [age, setAge] = useState('');
 
-    useEffect(() => {
-        getData();
-    }, []);
+  useEffect(() => {
+    getData();
+  }, []);
+  
+  const updateData = async () => {
+    if(name.length==0){
 
-    const getData = () => {
-        try {
-            db.transaction((tx) => {
-                tx.executeSql(
-                    "SELECT Name, Age FROM Users",
-                    [],
-                    (tx, results) => {
-                        var len = results.rows.length;
-                        if (len > 0) {
-                            var userName = results.rows.item(0).Name;
-                            var userAge = results.rows.item(0).Age;
-                            setName(userName);
-                            setAge(userAge);
-                        }
-                    }
-                )
-            })
-        } catch (error) {
+Alert.alert('Warning!','Please write your data.')
+    }else {
+        try{
+          var user = {
+            Name:name,
+
+          }
+            await AsyncStorage.mergeItem('UserData', JSON.stringify(user));
+            Alert.alert('Succes !', 'Your data has been updated.');
+        }catch (error) {
             console.log(error);
-        }
-    }
 
-    const updateData = async () => {
-        if (name.length == 0) {
-            Alert.alert('Warning!', 'Please write your data.')
-        } else {
-            try {
-                db.transaction((tx) => {
-                    tx.executeSql(
-                        "UPDATE Users SET Name=?",
-                        [name],
-                        () => { Alert.alert('Success!', 'Your data has been updated.') },
-                        error => { console.log(error) }
-                    )
-                })
-            } catch (error) {
-                console.log(error);
-            }
         }
-    }
 
-    const removeData = async () => {
-        try {
-            db.transaction((tx) => {
-                tx.executeSql(
-                    "DELETE FROM Users",
-                    [],
-                    () => { navigation.navigate('Login') },
-                    error => { console.log(error) }
-                )
-            })
-        } catch (error) {
-            console.log(error);
-        }
     }
-
-    return (
-        <View style={styles.body}>
-            <Text style={[
-                GlobalStyle.CustomFont,
-                styles.text
-            ]}>
-                Welcome {name} !
-            </Text>
-            <Text style={[
-                GlobalStyle.CustomFont,
-                styles.text
-            ]}>
-                Your age is {age}
-            </Text>
-            <TextInput
-                style={styles.input}
-                placeholder='Enter your name'
-                value={name}
-                onChangeText={(value) => setName(value)}
-            />
-            <CustomButton
-                title='Update'
-                color='#4e758a'
-                onPressFunction={updateData}
-            />
-            <CustomButton
-                title='Remove'
-                color='#4e758a'
-                onPressFunction={removeData}
-            />
-        </View>
-    )
 }
 
+const removeData = async () => {
+      try{
+          await AsyncStorage.removeItem('UserName');
+          navigation.navigate('Login');
+      }catch (error) {
+          console.log(error);
+
+      }
+
+  
+}
+
+const getData = () => {
+  try{
+AsyncStorage.getItem('UserData')
+.then(value=>{
+  if(value!=null){
+    let user = JSON.parse(value)
+    setName(user.Name);
+    setAge(user.Age);
+  }
+}
+
+)
+  }catch(error){
+console.log(error);
+
+  }
+}
+  return (
+    <View style = {styles.body}>
+      <Text style = {[
+        GlobalStyle.CustomFont,
+        styles.text
+      ]}>
+        Welcome {name} !
+      </Text>
+      <Text style = {[
+        GlobalStyle.CustomFont,
+        styles.text
+      ]}>
+        Your age is {age} 
+      </Text>
+      
+      <TextInput
+      style = {styles.input}
+      placeholder='Enter your name'
+      value={name}
+      onChangeText={(value) => setName(value)}
+      />
+      <CustomisedButton
+      title = 'Update'
+      color = '#5a9e6d'
+      onPressFunction = {updateData}
+      
+      />
+
+<CustomisedButton
+      title = 'Delete'
+      color = '#5a9e6d'
+      onPressFunction = {removeData}
+      
+      />
+    </View>
+      
+  )
+    }
+    
 const styles = StyleSheet.create({
     body: {
-        flex: 1,
-        alignItems: 'center',
+      flex: 1,
+      alignItems: 'center',
     },
     text: {
-        fontSize: 40,
-        margin: 10,
+      fontSize: 40,
+      margin: 10,
+     },
+    button: {
+      width: 150,
+      height: 50,
+      alignItems: 'center',
+    
     },
-    input: {
-        width: 300,
-        borderWidth: 1,
-        borderColor: '#555',
-        borderRadius: 10,
-        backgroundColor: '#ffffff',
-        textAlign: 'center',
-        fontSize: 20,
-        marginTop: 130,
-        marginBottom: 10,
-    }
-})
+    input:{
+      width: 300,
+      borderWidth:1,
+      borderColor:'#555',
+      borderRadius: 10,
+      backgroundColor: '#ffffff',
+      textAlign: 'center',
+      fontSize: 20,
+      marginTop:130,
+      marginBottom:10,
+  }
+  })
--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,133 +1,113 @@
-import React, { useState, useEffect } from 'react';
-import {
-    View,
-    StyleSheet,
-    Image,
-    Text,
-    TextInput,
-    Alert,
-} from 'react-native';
-import CustomButton from '../utils/CustomButton';
-import SQLite from 'react-native-sqlite-storage';
+import React, { useEffect, useState } from 'react';
+import { View, StyleSheet, Image, Alert} from 'react-native';
+import { TextInput } from 'react-native-gesture-handler';
+import { Text } from 'react-native-paper';
+import CustomisedButton from '../utils/CustomButton';
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
-const db = SQLite.openDatabase(
-    {
-        name: 'MainDB',
-        location: 'default',
-    },
-    () => { },
-    error => { console.log(error) }
-);
+export default function Login({navigation}){
 
-export default function Login({ navigation }) {
-
-    const [name, setName] = useState('');
-    const [age, setAge] = useState('');
+    const [name,setName] = useState('');
+    const [age,setAge] = useState('');
 
     useEffect(() => {
-        createTable();
         getData();
-    }, []);
-
-    const createTable = () => {
-        db.transaction((tx) => {
-            tx.executeSql(
-                "CREATE TABLE IF NOT EXISTS "
-                + "Users "
-                + "(ID INTEGER PRIMARY KEY AUTOINCREMENT, Name TEXT, Age INTEGER);"
-            )
-        })
-    }
-
+      }, []);
+      
     const getData = () => {
-        try {
-            db.transaction((tx) => {
-                tx.executeSql(
-                    "SELECT Name, Age FROM Users",
-                    [],
-                    (tx, results) => {
-                        var len = results.rows.length;
-                        if (len > 0) {
-                            navigation.navigate('Home');
-                        }
-                    }
-                )
-            })
-        } catch (error) {
-            console.log(error);
-        }
+      try{
+    AsyncStorage.getItem('UserName')
+    .then(value=>{
+      if(value!=null){
+        navigation.navigate('Home')
+      }
+    }
+    
+    )
+      }catch(error){
+    console.log(error);
+    
+      }
     }
 
     const setData = async () => {
-        if (name.length == 0 || age.length == 0) {
-            Alert.alert('Warning!', 'Please write your data.')
-        } else {
-            try {
-                await db.transaction(async (tx) => {
-                    await tx.executeSql(
-                        "INSERT INTO Users (Name, Age) VALUES (?,?)",
-                        [name, age]
-                    );
-                })
-                navigation.navigate('Home');
-            } catch (error) {
-                console.log(error);
+    if(name.length==0 || age.length == 0){
+
+Alert.alert('Warning!','Please write your data.')
+    }else {
+        try{
+            var user = {
+                Name: name,
+                Age: age,
             }
+            await AsyncStorage.setItem('UserData', JSON.stringify(user));
+            navigation.navigate('Home');
+        }catch (error) {
+            console.log(error);
+
         }
+
     }
-
-    return (
-        <View style={styles.body} >
-            <Image
-                style={styles.logo}
-                source={require('../../assets/Sqlite-square-icon.png')}
-            />
-            <Text style={styles.text}>
-
-            </Text>
-            <TextInput
-                style={styles.input}
-                placeholder='Enter your name'
-                onChangeText={(value) => setName(value)}
-            />
-            <TextInput
-                style={styles.input}
-                placeholder='Enter your age'
-                onChangeText={(value) => setAge(value)}
-            />
-            <CustomButton
-                title='Login'
-                color='#4e758a'
-                onPressFunction={setData}
-            />
-        </View>
-    )
 }
+return(
+    <View
+    style = {styles.body}>
+        <Image
+            style = {styles.logo}
+            source = {require('../../assets/logo.png')}
+            />
+<Text style = {styles.text}>
+    Async Storage
+</Text>
+<TextInput
+placeholder='Enter your name'
+style = {styles.input}
+onChangeText = {(value) => setName(value)}
 
-const styles = StyleSheet.create({
-    body: {
-        flex: 1,
-        alignItems: 'center',
-        backgroundColor: '#a1d3ed',
-    },
-    logo: {
-        width: 200,
-        height: 200,
-        margin: 20,
-    },
-    text: {
-        fontSize: 30,
-        color: '#ffffff',
-        marginBottom: 130,
-    },
-    input: {
-        width: 300,
-        borderWidth: 1,
-        borderColor: '#555',
-        borderRadius: 10,
-        backgroundColor: '#ffffff',
-        textAlign: 'center',
-        fontSize: 20,
-        marginBottom: 10,
-    }
+/>
+<TextInput
+placeholder='Enter your age'
+style = {styles.input}
+onChangeText = {(value) => setAge(value)}
+
+/>
+<CustomisedButton
+onPressFunction={setData}
+title = 'Login'
+color= '#5a9e6d'
+/>
+
+    </View>
+)
+
+}
+const styles = StyleSheet.create ({
+body :{
+    flex: 1,
+    alignItems: 'center',
+    backgroundColor: '#c1f5d0',
+
+},
+logo:{
+    width: 100,
+    height: 100,
+    margin:20,
+},
+text:{
+    fontSize:30,
+    color: '#1c4a29',
+    marginBottom:120,
+
+},
+input:{
+    width: 300,
+    borderWidth:1,
+    borderColor:'#555',
+    borderRadius: 10,
+    backgroundColor: '#ffffff',
+    textAlign: 'center',
+    fontSize: 20,
+    marginTop:10,
+    marginBottom:10,
+}
 })
